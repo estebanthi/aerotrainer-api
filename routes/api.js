03d73eff54ea5704988adcb0b373be6951bcc744
estebanthi <esteban.thilliez@gmail.com>
2024-09-13 16:42:34 +0200

    Auth

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {Pool} = require("pg");
+const {hash, compare} = require('bcrypt');
 const router = express.Router();
 
 const pool = new Pool({
@@ -101,6 +102,59 @@ router.get('/collections', async (req, res) => {
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
+
+router.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+router.post('/register', async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        if (!email || !password) {
+            res.status(400).json({error: 'Email ou mot de passe manquant'});
+            return;
+        }
+
+        const result = await pool.query('SELECT * FROM users WHERE username = $1', [email]);
+        if (result.rows.length > 0) {
+            res.status(400).json({error: 'Email déjà utilisé'});
+            return;
+        }
+
+        const hashedPassword = await hash(password, 10);
+        await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [email, hashedPassword]);
+        res.status(201).json({message: 'User created successfully'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Erreur interne du serveur'});
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        if (result.rows.length === 0) {
+            res.status(401).json({error: 'Invalid credentials'});
+            return;
+        }
+        const user = result.rows[0];
+        if (await compare(password, user.password)) {
+            res.status(200).json({id: user.id, username: user.username});
+        } else {
+            res.status(401).json({error: 'Invalid credentials'});
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
 
 
 module.exports = router;
